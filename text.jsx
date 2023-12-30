a92d1ec93a6710db08e409488f2880efc90d4821
Adinath Shelke <shelkeadinath3@gmail.com>
2023-12-30 21:38:22 +0530

    changed openai key

---
+++ text.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const text = () => {
+    const func = async () => {
+        const result = fetch("urls")
+        console.log(result);
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+        }
+    }
+    func()
+  return (
+    <div>text</div>
+  )
+}
+
+export default text
